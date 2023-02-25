08b4e900a8971237d15ae55ee0dae4ff9c385424
Pedro Lucas Freitas Sousa <Pedrofreitas16083@gmail.com>
2023-02-25 13:41:40 -0300

    Criando pastas básicas

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GraviBaby</title>
+</head>
+<body>
+    <?php include_once("./view/tela_login") ?>
+</body>
+</html>
